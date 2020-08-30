15297c78bdbd57d8562fd6b25fa856471191339b
Richard Darst <rkd@zgib.net>
2020-08-30 21:22:11 +0300

    Update files

--- conf.py
+++ conf.py
@@ -20,6 +20,9 @@
 project = 'LESSON NAME'
 copyright = '2020, The contributors'
 author = 'The contributors'
+github_user = 'coderefinery'
+github_repo_name = ''  # auto-detected from dirname if blank
+github_version = 'master/' # with trailing slash
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,15 +31,16 @@ author = 'The contributors'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_lesson',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,9 +48,20 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
+
+
+# HTML context:
+from os.path import dirname, realpath, basename
+html_context = {'display_github': True,
+                'github_user': github_user,
+                # Auto-detect directory name.  This can break, but
+                # useful as a default.
+                'github_repo': github_repo_name or basename(dirname(realpath(__file__))),
+                'github_version': github_version,
+               }
--- index.rst
+++ index.rst
@@ -1,20 +1,31 @@
-.. LESSON NAME documentation master file, created by
-   sphinx-quickstart on Sun Aug 30 20:59:48 2020.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+LESSON NAME
+===========
 
-Welcome to LESSON NAME's documentation!
-=======================================
+Intro
+
+.. prereq::
+
+   prerequisites
+
+.. csv-table::
+   :widths: auto
+
+   20 min , :doc:`filename`
+
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :maxdepth: 1
+   :caption: The lesson
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Reference
+
+   quick-reference
+   guide
 
 
 
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+See also
+--------
