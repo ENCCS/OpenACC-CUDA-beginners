af14fb51ad3cf4c27460b43b24860eec28a90cd0
Artem Zhmurov <zhmurov@gmail.com>
2021-04-27 15:24:05 +0300

    Add cuda implementations fot heat equation

---
+++ examples/CUDA/HeatEquation/heat_equation_gpu_2_remove_copy_calls.cu
@@ -0,0 +1,134 @@
+/* 2D heat equation
+
+   Copyright (C) 2014  CSC - IT Center for Science Ltd.
+
+*/
+
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pngwriter.h"
+
+#define BLOCK_SIZE_X 16
+#define BLOCK_SIZE_Y 16
+
+/* Convert 2D index layout to unrolled 1D layout
+ *
+ * \param[in] i      Row index
+ * \param[in] j      Column index
+ * \param[in] width  The width of the area
+ * 
+ * \returns An index in the unrolled 1D array.
+ */
+int __host__ __device__ getIndex(const int i, const int j, const int width)
+{
+    return i*width + j;
+}
+
+__global__ void evolve_kernel(const float* Un, float* Unp1, const int nx, const int ny, const float dx2, const float dy2, const float aTimesDt)
+{
+    int i = threadIdx.x + blockIdx.x*blockDim.x;
+    if (i > 0 && i < nx - 1)
+    {
+        int j = threadIdx.y + blockIdx.y*blockDim.y;
+        if (j > 0 && j < ny - 1)
+        {
+            const int index = getIndex(i, j, nx);
+            float uij = Un[index];
+            float uim1j = Un[getIndex(i-1, j, nx)];
+            float uijm1 = Un[getIndex(i, j-1, nx)];
+            float uip1j = Un[getIndex(i+1, j, nx)];
+            float uijp1 = Un[getIndex(i, j+1, nx)];
+
+            // Explicit scheme
+            Unp1[index] = uij + aTimesDt * ( (uim1j - 2.0*uij + uip1j)/dx2 + (uijm1 - 2.0*uij + uijp1)/dy2 );
+        }
+    }
+}
+
+int main()
+{
+    const int nx = 200;   // Width of the area
+    const int ny = 200;   // Height of the area
+
+    const float a = 0.5;     // Diffusion constant
+
+    const float dx = 0.01;   // Horizontal grid spacing 
+    const float dy = 0.01;   // Vertical grid spacing
+
+    const float dx2 = dx*dx;
+    const float dy2 = dy*dy;
+
+    const float dt = dx2 * dy2 / (2.0 * a * (dx2 + dy2)); // Largest stable time step
+    const int numSteps = 500;                             // Number of time steps
+    const int outputEvery = 100;                          // How frequently to write output image
+
+    int numElements = nx*ny;
+
+    // Allocate two sets of data for current and next timesteps
+    float* h_Un   = (float*)calloc(numElements, sizeof(float));
+
+    // Initializing the data with a pattern of disk of radius of 1/6 of the width
+    float radius2 = (nx/6.0) * (nx/6.0);
+    for (int i = 0; i < nx; i++)
+    {
+        for (int j = 0; j < ny; j++)
+        {
+            int index = getIndex(i, j, nx);
+            // Distance of point i, j from the origin
+            float ds2 = (i - nx/2) * (i - nx/2) + (j - ny/2)*(j - ny/2);
+            if (ds2 < radius2)
+            {
+                h_Un[index] = 5.0;
+            }
+            else
+            {
+                h_Un[index] = 65.0;
+            }
+        }
+    }
+
+    float* d_Un;
+    float* d_Unp1;
+
+    cudaMalloc((void**)&d_Un, numElements*sizeof(float));
+    cudaMalloc((void**)&d_Unp1, numElements*sizeof(float));
+
+    cudaMemcpy(d_Un, h_Un, numElements*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_Unp1, h_Un, numElements*sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 numBlocks(nx/BLOCK_SIZE_X + 1, ny/BLOCK_SIZE_Y + 1);
+    dim3 threadsPerBlock(BLOCK_SIZE_X, BLOCK_SIZE_Y);
+
+    // Main loop
+    for (int n = 0; n < numSteps; n++)
+    {
+        if (n % 2 == 0)
+        {
+            evolve_kernel<<<numBlocks, threadsPerBlock>>>(d_Un, d_Unp1, nx, ny, dx2, dy2, a*dt);
+        }
+        else
+        {
+            evolve_kernel<<<numBlocks, threadsPerBlock>>>(d_Unp1, d_Un, nx, ny, dx2, dy2, a*dt);
+        }
+
+        // Write the output if needed
+        if (n % outputEvery == 0)
+        {
+            cudaMemcpy(h_Un, d_Un, numElements*sizeof(float), cudaMemcpyDeviceToHost);
+            char filename[64];
+            sprintf(filename, "heat_%04d.png", n);
+            save_png(h_Un, nx, ny, filename, 'c');
+        }
+    }
+
+    // Release the memory
+    free(h_Un);
+
+    cudaFree(d_Un);
+    cudaFree(d_Unp1);
+    
+    return 0;
+}
