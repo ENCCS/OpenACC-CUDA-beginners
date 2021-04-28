c5c115e5a979719dbde031425c8836cb38032f46
Jing Gong <x_jingo@tetralith2.nsc.liu.se>
2021-04-28 20:44:33 +0200

    Clear some files

--- examples/OpenACC/HeatEquation/c/heat_equation.c
+++ examples/OpenACC/HeatEquation/c/heat_equation.c
@@ -89,13 +89,10 @@ int main()
     memcpy(Unp1[0], Un[0], numElements*sizeof(float));
 
     // Main loop
-#pragma acc data copyin(Un[0:nx][0:ny]) create(Unp1[0:nx][0:ny])
-{
     for (int n = 0; n < numSteps; n++)
     {
         // Going through the entire area
-      //#pragma acc parallel loop copyin(Un[0:nx][0:ny]) copyout(Unp1[0:nx][0:ny]) 
-#pragma acc parallel loop collapse(2)
+        /* TODO: Implement computation on device with OpenACC */
         for (int i = 1; i < nx-1; i++)
         {
             for (int j = 1; j < ny-1; j++)
@@ -111,19 +108,11 @@ int main()
         {
             char filename[64];
             sprintf(filename, "heat_%04d.png", n);
-#pragma acc update host(Un[0:nx][0:ny])
             save_png(Un[0], nx, ny, filename, 'c');
         }
-        // Copy the pointers for the next timestep
-	//memcpy(Un[0], Unp1[0], numElements*sizeof(float));
-#pragma acc parallel loop collapse(2)
-        for (int i = 1; i < nx; i++) 
-	{
-	  for (int j = 1; j < ny; j++)
-	    Un[i][j] = Unp1[i][j];
-	}
+        // Swapping the pointers for the next timestep
+	memcpy(Un[0], Unp1[0], numElements*sizeof(float));
     }
-}
 
     // Release the memory
     free_2d(Un);
