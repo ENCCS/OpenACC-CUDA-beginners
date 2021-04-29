7e211eb4f1bc2462bd73c59984d87dc1c5529f7c
Jing Gong <x_jingo@tetralith2.nsc.liu.se>
2021-04-29 16:35:58 +0200

    minor update on examples

---
+++ examples/OpenACC/vector-sum/solution/F90/sum_parallel_call_function.F90
@@ -0,0 +1,42 @@
+program vectorsum
+#ifdef _OPENACC
+  use openacc
+#endif
+  implicit none
+  integer, parameter :: rk = selected_real_kind(12)
+  integer, parameter :: ik = selected_int_kind(9)
+  integer, parameter :: nx = 102400
+
+  real(kind=rk), dimension(nx) :: vecA, vecB, vecC
+  real(kind=rk)    :: sum
+  integer(kind=ik) :: i
+
+  ! Initialization of vectors
+  do i = 1, nx
+     vecA(i) = 1.0_rk/(real(nx - i + 1, kind=rk))
+     vecB(i) = vecA(i)**2
+  end do
+
+  !$acc parallel
+  !$acc loop
+  do i = 1, nx
+     vecC(i) = mul(vecA(i), vecB(i))
+  end do
+  !$acc end loop
+  !$acc end parallel loop
+
+  ! Compute the check value
+  write(*,*) 'Reduction sum: ', sum(vecC)
+
+contains
+
+  real(kind=rk) function mul(a, b)
+!$acc routine seq
+    implicit none
+    real(kind=rk), intent(in) :: a, b
+    
+    mul = a * b
+    return 
+  end function mul
+  
+end program vectorsum
---
+++ examples/OpenACC/vector-sum/solution/c/sum_parallel_call_function.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#ifdef _OPENACC
+#include <openacc.h>
+#endif
+
+#define NX 102400
+
+// return a*b;
+#pragma acc routine seq
+double mul(double a, double b)
+{
+    return a * b;
+}
+
+int main(void)
+{
+    double vecA[NX], vecB[NX], vecC[NX];
+    double sum;
+    int i;
+
+    /* Initialization of the vectors */
+    for (i = 0; i < NX; i++) {
+        vecA[i] = 1.0 / ((double) (NX - i));
+        vecB[i] = vecA[i] * vecA[i];
+    }
+
+#pragma acc parallel
+    {
+#pragma acc loop
+        for (i = 0; i < NX; i++) {
+            vecC[i] = mul(vecA[i], vecB[i]);
+        }
+    }
+
+    sum = 0.0;
+    /* Compute the check value */
+    for (i = 0; i < NX; i++) {
+        sum += vecC[i];
+    }
+    printf("Reduction sum: %18.16f\n", sum);
+
+    return 0;
+}
